cdc22882c9cb1cd251f00542b63113a35c995494
Alex <minecraftiantimelord42@gmail.com>
2018-05-23 14:29:35 -0500

    player stuffs
    shtuffs

--- app/src/main/java/com/example/klugiewicza/textbasedadventure/Player.java
+++ app/src/main/java/com/example/klugiewicza/textbasedadventure/Player.java
@@ -5,11 +5,15 @@ public class Player
 {
     //playerproperties
     public String PlayerName = "";
-    public int health = 100;
-    public int dex = 10;
+    public int hp = 100;
+    public int ac = 10;
     public int str = 10;
+    public int dex = 10;
+    public int con = 10;
+    public int wis = 10;
+    public int intl = 10;
     public int luck = 10;
-    public int Wizardness = 10;
+    
     public ArrayList<String> Inventory = new ArrayList<String>();
     public ArrayList<String> StatusEffects = new ArrayList<String>();
 }
